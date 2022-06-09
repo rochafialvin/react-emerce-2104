5a237ce3752650408d698dc2d921274561eabbbb
rochafialvin <rochafi.dev@gmail.com>
2022-06-09 10:33:21 +0700

    Init router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <h1>App Component</h1>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;

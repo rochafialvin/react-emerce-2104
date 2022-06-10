70d8b2d305cbc9d877c693117ceb295863d70633
rochafialvin <rochafi.dev@gmail.com>
2022-06-10 11:33:24 +0700

    Create product list

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+
+import "./style.css";
+
+function ProductCard({ product }) {
+  const { productName, productImage, price } = product;
+  return (
+    <div className="card product-card">
+      <img src={productImage} alt="" />
+      <div className="mt-2">
+        <div>
+          <h5>{productName}</h5>
+          <span className="text-muted">{price}</span>
+        </div>
+        <div className="d-flex flex-row justify-content-end">
+          <Button
+            className="mt-2 w-100"
+            outline
+            color="primary"
+            tag={Link}
+            to="/note"
+          >
+            Detail
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,42 @@
-import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ProductCard from "../../components/ProductCard";
+import axiosInstance from "../../services/axios";
 
 function Home() {
-  return <h1>Home Page</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchProducts();
+    }, 1000);
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const resGetProducts = await axiosInstance.get("/products");
+      setProducts(resGetProducts.data);
+    } catch (error) {
+      alert("Terjadi kesalahan");
+      console.log({ error });
+    }
+  };
+
+  const renderProducts = () => {
+    return products.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ));
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3 bg-success ">
+          <h1>filtering</h1>
+        </div>
+        <div className="col-9 d-flex flex-wrap ">{renderProducts()}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

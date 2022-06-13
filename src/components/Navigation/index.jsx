80c1c1f398bfa1748e2dd10b9e19a831332140fb
rochafialvin <rochafi.dev@gmail.com>
2022-06-13 14:00:22 +0700

    Access value from state

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -11,29 +11,48 @@ import {
 } from "reactstrap";
 
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navigation() {
+  const { id, username } = useSelector((state) => state.auth);
+
   return (
     <Navbar color="dark" dark expand="md">
       <NavbarBrand tag={Link} to="/">
         EMERCE 2104
       </NavbarBrand>
       <Nav className="me-auto" navbar>
-        <NavItem>
-          <NavLink tag={Link} to="/login">
-            Login
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to="/register">
-            Register
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={Link} to="/note">
-            Note
-          </NavLink>
-        </NavItem>
+        {id ? (
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              Hello, {username}
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Cart</DropdownItem>
+              <DropdownItem>Transaction</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Logout</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        ) : (
+          <>
+            <NavItem>
+              <NavLink tag={Link} to="/login">
+                Login
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/register">
+                Register
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/note">
+                Note
+              </NavLink>
+            </NavItem>
+          </>
+        )}
       </Nav>
     </Navbar>
   );

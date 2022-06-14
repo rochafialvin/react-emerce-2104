646ce8f6b934ecc7516a3b62c6fc78c54cd8aa95
rochafialvin <rochafi.dev@gmail.com>
2022-06-14 15:10:15 +0700

    Get product detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,24 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axiosInstance from "../../services/axios";
 
 function DetailProduct() {
   // params.productId --> gunakan untuk ambil data
   const params = useParams();
+  const [product, setProduct] = useState({});
+
+  const fetchProduct = async () => {
+    try {
+      const res = await axiosInstance.get(`/products/${params.productId}`);
+      setProduct(res.data);
+    } catch (error) {
+      alert("Terjadi kesalahan");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const { productImage, productName, price, description } = product;
   return (
     <div className="container">
       <div className="row mt-3">
         <div className="col-6">
-          <img
-            style={{ width: "100%" }}
-            src={
-              "https://d29c1z66frfv6c.cloudfront.net/pub/media/catalog/product/large/35986529ca3dc88d58695fe1396f909aaf0eea57_xxl-1.jpg"
-            }
-            alt="tulisan"
-          />
+          <img style={{ width: "100%" }} src={productImage} alt="tulisan" />
         </div>
         <div className="col-6 d-flex flex-column justify-content-center">
-          <h4>Kaos Baja Hitam</h4>
-          <h5>Rp 20.000</h5>
-          <p>Kaos baja hitam pembela kebenaran</p>
+          <h4>{productName}</h4>
+          <h5>Rp {price?.toLocaleString("id")}</h5>
+          <p>{description}</p>
           <div className="d-flex flex-row align-items-center">
             <button className="btn btn-primary ">-</button>
             <strong className="text-center mx-4">30</strong>

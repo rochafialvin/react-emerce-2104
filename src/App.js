8202c41be7c53518790482a4c25f2aaff086c237
rochafialvin <rochafi.dev@gmail.com>
2022-06-14 10:49:16 +0700

    Implement Keep Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { login } from "./store/slices/authSlice";
+import { useDispatch } from "react-redux";
 
 // NOTES
 import InputBox from "./components/InputBox";
@@ -13,17 +16,39 @@ import Register from "./pages/Register";
 import UseEffectComponent from "./notes/useEffect";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/note" element={<InputBox />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const [isLocalStorageChecked, setisLocalStorageChecked] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setTimeout(() => {
+      const userLocalStorage = localStorage.getItem("userInfo");
+
+      if (userLocalStorage) {
+        // simpan ke global state
+        const user = JSON.parse(userLocalStorage);
+        const action = login(user);
+        dispatch(action);
+      }
+
+      setisLocalStorageChecked(true);
+    }, 1000);
+  }, []);
+
+  if (isLocalStorageChecked) {
+    return (
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/note" element={<InputBox />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
+  return <h1 style={{ textAlign: "center" }}>Checking Local Storage</h1>;
 }
 export default App;
